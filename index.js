0e1733ebbabb958c88cfce0db35075bf35d70b18
Henrik Wingerei <henrik.wingerei@bekk.no>
2015-05-06 21:29:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var request = require('request');
+var express = require('express');
+var moment = require('moment');
+
+var app = express();
+
+app.get('/api', function(req, res) {
+
+	request("http://reisapi.ruter.no/stopvisit/getdepartures/2190090", function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var departures = JSON.parse(body)
+			var expectedDep = moment(departures[0].MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime);
+			var minutesToNextDeparture = moment.duration(expectedDep - moment()).minutes();
+
+			var result = {value: minutesToNextDeparture};
+			res.json(JSON.stringify(result));
+		}
+	});
+
+})
+
+app.use("/", express.static("public"));
+
+
+app.set('port', (process.env.PORT || 8000));
+var server = app.listen(app.get('port'), function() {
+	console.log("Starting...");
+})
